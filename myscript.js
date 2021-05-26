b8b127437e419844f6f625bb6030f8cdb60ac7df
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-05-26 14:53:35 +0200

    lowercase

--- myscript.js
+++ myscript.js
@@ -2,4 +2,4 @@ var nome = prompt("Inserisci il tuo nome");
 var cognome = prompt("Inserisci il tuo cognome");
 var colore = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById('text').innerHTML = nome + cognome + colore + '21';
+document.getElementById('text').innerHTML = (nome + cognome + colore).toLowerCase() + '21';
